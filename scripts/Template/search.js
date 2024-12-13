fdbf35a6051c971dc9afe180288a811af2b00f81
Svenhildr <julie.poissonnier.1989@gmail.com>
2024-12-13 18:38:06 +0100

    fixes

--- scripts/Template/search.js
+++ scripts/Template/search.js
@@ -1,3 +1,51 @@
+import { cardDisplay } from "../index.js";
+import { filtersList } from "../tag.js";
+
+export let filteredRecipesUpdate = [];
+
+/**
+ * Attaches an event listener to the main search bar to filter recipes when the input value changes.
+ *
+ * @function mainFilter
+ * @param {Array} recipes - An array of recipe objects
+ * @returns {void}
+ */
+export default function mainFilter(recipes) {
+    const mainSearchbar = document.getElementById("searchbar");
+    mainSearchbar.addEventListener("input", (event) => {
+        event.preventDefault();
+        search(recipes);
+    });
+}
+
+/**
+ * Searches for recipes based on the input value in the search bar and updates the filtered recipes list accordingly.
+ *
+ * @function search
+ * @param {Array} recipes - An array of recipe objects
+ * @returns {void}
+ */
+export function search(recipes) {
+    const mainSearchbar = document.getElementById("searchbar");
+    let filteredRecipesTemp = filtersList.length === 0 ? recipes : filteredRecipesUpdate;
+    filteredRecipesUpdate = [];
+    if (mainSearchbar.value.length >= 3) {
+        filteredRecipesTemp = recipes.filter((recipe) => {
+            let recipeNameMatch = recipe.name.toLowerCase().includes(mainSearchbar.value);
+            let recipeIngredientsMatch = recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(mainSearchbar.value));
+            let recipeDescriptionMatch = recipe.description.toLowerCase().includes(mainSearchbar.value);
+            return recipeIngredientsMatch || recipeNameMatch || recipeDescriptionMatch;
+        });
+
+        //assure que les recettes filtrées dans filteredRecipesTemp ne contiennent pas celles qui sont déjà présentes dans filtersList
+        filteredRecipesUpdate = filteredRecipesTemp.filter((recipe) => !filtersList.some((existingRecipe) => existingRecipe.id === recipe.id));
+    } else {
+        filteredRecipesUpdate = filtersList.length === 0 ? recipes : filteredRecipesUpdate;
+    }
+
+    filterUpdate(false, true);
+}
+
 /**
  * Updates the filtered recipes based on the applied tags (ingredients, appliances, ustensils).
  * @param {Array} filteredRecipesTemp The array of recipes filtered by the search query.
